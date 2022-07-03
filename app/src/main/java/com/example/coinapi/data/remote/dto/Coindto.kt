f606c5399a5060603f04309f4e01b65dc4eb690c
Stephany Paulino Balbi <stephany_paulino1@ucne.edu.do>
2022-07-03 13:31:18 -0400

    Inicializando valores

--- app/src/main/java/com/example/coinapi/data/remote/dto/Coindto.kt
+++ app/src/main/java/com/example/coinapi/data/remote/dto/Coindto.kt
@@ -1,8 +1,8 @@
 package com.example.coinapi.data.remote.dto
 
 data class Coindto (
-    val monedaId: Int,
-    val descripcion: String ?,
-    val valor: Double ?,
-    val imagenUrl: String?
+    val monedaId: Int = 0,
+    val descripcion: String,
+    val valor: Double = 0.0,
+    val imagenUrl: String? = ""
 )
