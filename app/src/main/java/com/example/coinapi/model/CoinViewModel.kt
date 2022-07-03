8e44841af98acf48f7e9e1fce0b28c5dac1bad8f
Stephany Paulino Balbi <stephany_paulino1@ucne.edu.do>
2022-07-03 13:32:37 -0400

    Implementando funcionalidad del guardar

--- app/src/main/java/com/example/coinapi/model/CoinViewModel.kt
+++ app/src/main/java/com/example/coinapi/model/CoinViewModel.kt
@@ -1,15 +1,19 @@
 package com.example.coinapi.model
 
 import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.coinapi.data.remote.ListStateCoin
+import com.example.coinapi.data.remote.dto.Coindto
 import com.example.coinapi.data.remote.repository.CoinRepository
 import com.example.coinapi.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
@@ -21,15 +25,19 @@ class CoinViewModel @Inject constructor(
     private var _state = mutableStateOf(ListStateCoin())
     val state: State<ListStateCoin> = _state
 
-    init {
-        coinRepository.getExchanges().onEach { result ->
+    var descripcion by mutableStateOf("")
+    var valor by mutableStateOf("")
+
+    fun Get()
+    {
+        coinRepository.getCoin().onEach { result ->
             when (result) {
                 is Resource.Loading -> {
                     _state.value = ListStateCoin(isLoading = true)
                 }
 
                 is Resource.Success -> {
-                    _state.value = ListStateCoin(exchange = result.data ?: emptyList())
+                    _state.value = ListStateCoin(coins = result.data ?: emptyList())
                 }
                 is Resource.Error -> {
                     _state.value = ListStateCoin(error = result.message ?: "Error desconocido")
@@ -37,4 +45,21 @@ class CoinViewModel @Inject constructor(
             }
         }.launchIn(viewModelScope)
     }
+
+    fun Guardar(){
+        viewModelScope.launch {
+            coinRepository.postCoin(
+                Coindto(
+                    valor = valor.toDouble(),
+                    descripcion = descripcion
+                )
+            )
+        }
+    }
+
+    init {
+        Get()
+    }
+
+
 }
