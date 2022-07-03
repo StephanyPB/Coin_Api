a7c7637706da22ae7e3a355d810c1589c54f8daf
Stephany Paulino Balbi <stephany_paulino1@ucne.edu.do>
2022-07-03 13:32:08 -0400

    Agregando Funcionalidad del post

--- app/src/main/java/com/example/coinapi/data/remote/repository/CoinRepository.kt
+++ app/src/main/java/com/example/coinapi/data/remote/repository/CoinRepository.kt
@@ -6,19 +6,21 @@ import com.example.coinapi.util.Resource
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import retrofit2.HttpException
+import retrofit2.Response
 import java.io.IOException
+import java.lang.Exception
 import javax.inject.Inject
 
 class CoinRepository @Inject constructor(
     private val coinAPI: CoinAPI
-){
-    fun getExchanges(): Flow<Resource<List<Coindto>>> = flow {
+) {
+    fun getCoin(): Flow<Resource<List<Coindto>>> = flow {
         try {
             emit(Resource.Loading()) //indicar que estamos cargando
 
-            val coins = coinAPI.getExchange() //descarga las monedas de internet, se supone quedemora algo
+            val coins = coinAPI.getCoin() //descarga las monedas de interneto
 
-            emit(Resource.Success(coins)) //indicar que se cargo correctamente y pasarle las monedas
+            emit(Resource.Success(coins)) //se cargo correctamente y pasarle las monedas
         } catch (e: HttpException) {
             //error general HTTP
             emit(Resource.Error(e.message ?: "Error HTTP GENERAL"))
@@ -26,5 +28,15 @@ class CoinRepository @Inject constructor(
             //debe verificar tu conexion a internet
             emit(Resource.Error(e.message ?: "verificar tu conexion a internet"))
         }
+    }
+
+    suspend fun postCoin(coindto: Coindto): Coindto {
+        var result = Coindto(0,"",0.0)
+        try {
+             result =  coinAPI.postCoins(coindto)
+        } catch (e: Exception) {
+            System.out.println(e.message)
+        }
+        return result
     }
 }
